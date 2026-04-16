f8d6a8eff918020890ff08f3a16853baf7f6ba91
voldemore0509 <vdchanudet@gmail.com>
2026-04-16 23:28:16 +0200

    Re organisation

--- core/coc_pipeline.h
+++
@@ -1,4 +0,0 @@
-#ifndef COC_PIPELINE_H  //permet de dire que la fonction exsiste et quelle peut être appeler par un programme 1 fois
-#define COC_PIPELINE_H  //permet de dire que la fonction exsiste et quelle peut être appeler par un programme 1 fois
-void run_coc();
-#endif  //permet de signialer la fin
--- tests/testplace.py
+++
@@ -1,9 +0,0 @@
-class Testplace:
-    def written(self):
-        written = input("Enter your message : ")
-        print("reponse enter",written)
-        return written
-    
-#OBJECT
-testplace = Testplace()
-written = testplace.written()
