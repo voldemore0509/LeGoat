4dc34b6a6b70b9e83fb12de699a45c5948cf3644
voldemore0509 <vdchanudet@gmail.com>
2026-04-19 23:36:03 +0200

    Update generation.c

--- core/generation.c
+++ core/generation.c
@@ -1,7 +1,28 @@
 //Code for generating the response
 #include <stdio.h>
+#include <curl/curl.h>
+
+struct variable_generation
+{
+        char json_data[512];  // le buffer
+}
 
 void generation_prompt(struct model_config *models)
 {
-    //code pour la génération de réponse par defaut 
+    //initialisation
+    CURL *curl = curl_easy_init(); //appel la fonction
+    if(curl == NULL)
+    {
+        printf("Error Serve");
+    }
+    else
+    {
+        struct variable_generation *generation = malloc(sizeof(struct variable_generation)); //alloue de la mémoire pour stoquer et libérer à la fin
+        //permet de stoquer les paramètre JSON
+        snprintf(generation->json_data, sizeof(generation->json_data), 
+        "{\"model\":\"%s\", \"prompt\": \"%s\", \"stream\": false}", 
+        models->model, models->prompt);
+        free(generation);
+    }
+
 }
